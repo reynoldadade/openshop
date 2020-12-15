e363402e6a980d1660bd1e0150fa43013adf6029
Reynold Osei Adade <Reynold@walulel.com>
2020-12-15 11:43:03 +0000

    images effect

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -1,3 +1,19 @@
+import styles from "../styles/ProductDisplay.module.css";
+
+const url1 =
+	"https://images.unsplash.com/photo-1607732994987-15674d81ba62?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDF8dG93SlpGc2twR2d8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
+const url2 =
+	"https://images.unsplash.com/photo-1607899668381-8ead31f1900b?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDJ8dG93SlpGc2twR2d8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60";
 export default function ProductDisplay(props) {
-	return <div className='border-2 border-gray-300'></div>;
+	return (
+		<div className={`flex ${styles.currentImage}`}>
+			{/* images prefered are  500h x 750w px */}
+			<div className='border-1 border-gray-100'>
+				<img src={url1} className={styles.currentImage}></img>
+			</div>
+			<div className={`${styles.nextImageContainer}`}>
+				<img src={url2} className={styles.nextImage}></img>
+			</div>
+		</div>
+	);
 }
